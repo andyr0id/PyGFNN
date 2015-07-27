f7a2650164a838e24f849cca33963eec6215a865
Andy Lambert <andy@andyroid.co.uk>
2015-07-27 15:27:42 +0100

    adding gain to identity connections

--- pygfnn/structure/connections/absidentity.py
+++ pygfnn/structure/connections/absidentity.py
@@ -1,10 +1,14 @@
 __author__ = 'Andrew J. Lambert, andy@andyroid.co.uk'
 
 from pybrain import IdentityConnection
+from pygfnn.structure.connections.gain import GainConnection
 
 import numpy as np
 
-class AbsIdentityConnection(IdentityConnection):
+class AbsIdentityConnection(GainConnection, IdentityConnection):
+
+    def __init__(self, inmod, outmod, gain=1.0, **kwargs):
+        GainConnection.__init__(self, inmod, outmod, gain=gain, **kwargs)
 
     def _forwardImplementation(self, inbuf, outbuf):
-        outbuf += np.abs(inbuf)
+        outbuf += np.abs(inbuf) * self.gain
--- pygfnn/structure/connections/realidentity.py
+++ pygfnn/structure/connections/realidentity.py
@@ -1,10 +1,14 @@
 __author__ = 'Andrew J. Lambert, andy@andyroid.co.uk'
 
 from pybrain import IdentityConnection
+from pygfnn.structure.connections.gain import GainConnection
 
 import numpy as np
 
-class RealIdentityConnection(IdentityConnection):
+class RealIdentityConnection(GainConnection, IdentityConnection):
+
+    def __init__(self, inmod, outmod, gain=1.0, **kwargs):
+        GainConnection.__init__(self, inmod, outmod, gain=gain, **kwargs)
 
     def _forwardImplementation(self, inbuf, outbuf):
-        outbuf += np.real(inbuf)
+        outbuf += np.real(inbuf) * self.gain
