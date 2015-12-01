c00879ba62bbc245e8f6687ea0d9e0f81eb94255
Andy Lambert <andy@andyroid.co.uk>
2015-12-01 11:13:11 +0000

    changes to phase output

--- pygfnn/structure/connections/absphaseidentity.py
+++ pygfnn/structure/connections/absphaseidentity.py
@@ -5,14 +5,21 @@ from pygfnn.structure.connections.gain import GainConnection
 
 import numpy as np
 
+TWO_PI = np.pi*2
+R_TWO_PI = 1/TWO_PI
+
 class AbsPhaseIdentityConnection(GainConnection, IdentityConnection):
 
-    def __init__(self, inmod, outmod, gain=1.0, **kwargs):
-    	if outmod.dim < inmod.dim*2:
-    		raise ValueError('outmod.dim neds to be >= inmod.dim*2')
+    phasegain = R_TWO_PI
+
+    def __init__(self, inmod, outmod, gain=1.0, phasegain=R_TWO_PI, **kwargs):
+        if outmod.dim < inmod.dim*2:
+            raise ValueError('outmod.dim neds to be >= inmod.dim*2')
         GainConnection.__init__(self, inmod, outmod, gain=gain, **kwargs)
+        self.phasegain = phasegain
+        self.setArgs(phasegain = self.phasegain)
 
     def _forwardImplementation(self, inbuf, outbuf):
-    	length = len(inbuf)
+        length = len(inbuf)
         outbuf[:length] += np.abs(inbuf) * self.gain
-        outbuf[length:length*2] += np.angle(inbuf)
+        outbuf[length:length*2] += ((np.angle(inbuf) + TWO_PI) % TWO_PI) * self.phasegain
--- pygfnn/structure/connections/phaseidentity.py
+++ pygfnn/structure/connections/phaseidentity.py
@@ -5,10 +5,13 @@ from pygfnn.structure.connections.gain import GainConnection
 
 import numpy as np
 
+TWO_PI = np.pi*2
+R_TWO_PI = 1/TWO_PI
+
 class PhaseIdentityConnection(GainConnection, IdentityConnection):
 
-    def __init__(self, inmod, outmod, gain=1.0, **kwargs):
+    def __init__(self, inmod, outmod, gain=R_TWO_PI, **kwargs):
         GainConnection.__init__(self, inmod, outmod, gain=gain, **kwargs)
 
     def _forwardImplementation(self, inbuf, outbuf):
-        outbuf += np.angle(inbuf) * self.gain
+        outbuf += ((np.angle(inbuf) + TWO_PI) % TWO_PI) * self.gain
